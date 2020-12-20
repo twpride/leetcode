7d8cc42742e5d5080b370df20477b014ce138fae
howard <twpride@gmail.com>
2020-12-20 01:07:31 -0800

    saturday contest

---
+++ lc0005 -  Longest Palindromic Substring.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        def ispal(lo,hi):
+            while lo-1>=0 and hi+1<len(s) and s[lo-1]==s[hi+1]:
+                lo-=1
+                hi+=1
+            return s[lo:hi+1]
+        
+        best=1
+        ans=s[0]
+        for i in range(len(s)):
+            
+            cur = ispal(i,i)
+            if len(cur)>best:
+                ans=cur
+                best=len(cur)
+            
+            cur = ispal(i+1,i)
+            if len(cur)>best:
+                ans=cur
+                best=len(cur)
+        
+        return ans
+        
